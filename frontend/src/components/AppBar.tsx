48957490aafbae2d1342ab3e31952a3afd689b6f
abhijitroy0356 <60839264+abhijitroy0356@users.noreply.github.com>
2025-03-19 02:41:11 +0530

    added id

--- frontend/src/components/AppBar.tsx
+++ frontend/src/components/AppBar.tsx
@@ -1,10 +1,12 @@
 import Avatar from "./Avatar"
-
+import { Link } from "react-router-dom"
 
 function AppBar() {
   return (
     <div className=" bg-black border-b text-white flex justify-between px-10 py-6 items-center ">
+        <Link to={'/blogs'}>
         <div className=" border border-white p-4 md:text-3xl font-extrabold">Medium</div>
+        </Link>
         <div className="border border-white cursor-pointer rounded">
             <Avatar Name={"ab"} size={30}/>
         </div>
--- frontend/src/components/BlogTile.tsx
+++ frontend/src/components/BlogTile.tsx
@@ -1,31 +1,45 @@
-interface blog{
-    title:string;
-    content:string;
-    author:{
-        name:string
-    }
+import AppBar from "./AppBar";
+import Avatar from "./Avatar";
+
+interface Blog {
+    title: string;
+    content: string;
+    author: {
+        name: string;
+    };
 }
 
-function BlogTile({title, content, author}:blog) {
-    
-  return (
-<div>
-    <div>
-        <div>{title}</div>
-            <div>
-                <div>date:2/02/2013</div>
-                <div>{content}</div>
-            </div>
-        </div>
+function BlogTile({ title, content, author }: Blog) {
+    return (
         <div>
-            <div>Author{author.name}</div>
-            <div>
-                <div>Jokester</div>
-                <div>master of node.js in couple of days</div>
+            <AppBar/>
+        <div className="bg-white shadow-lg rounded-lg p-6 border border-gray-200 max-w-2xl mx-auto">
+    
+            {/* Title Section */}
+            <div className="mb-4 border-b pb-2">
+                <h2 className="text-2xl font-bold text-gray-900">{title}</h2>
+            </div>
+
+            {/* Content Section */}
+            <div className="text-gray-700 space-y-4">
+                <p className="text-sm text-gray-500">📅 Date: 02/02/2013</p>
+                <p className="text-lg">{content}</p>
+            </div>
+
+            {/* Author Info */}
+            <div className="mt-6 flex items-center gap-4 border-t pt-4">
+                <div className="flex flex-col">
+                    <span className="text-sm text-gray-500 "><Avatar Name={author.name ? author.name:"idsk"} size={20}/> Author:</span>
+                    <span className="text-md font-semibold text-gray-800">{author.name}</span>
+                </div>
+                <div className="text-sm text-gray-600">
+                    <p>Jokester</p>
+                    <p>Master of Node.js in a couple of days</p>
+                </div>
             </div>
         </div>
-</div>
-  )
+        </div>
+    );
 }
 
-export default BlogTile
+export default BlogTile;
